a0afa674fa91b77a3402952e497dec63acce7f38
Sudhindra Chandrashekar <sudhindra.chandrashekar@sap.com>
2020-12-15 19:27:57 +0530

    Initial Commit

---
+++ gameRanking/src/main/java/com/sudhi/gameranking/GameRankingApplication.java
@@ -0,0 +1,13 @@
+package com.sudhi.gameranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRankingApplication.class, args);
+	}
+
+}
---
+++ gameRanking/src/test/java/com/sudhi/gameranking/GameRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhi.gameranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
